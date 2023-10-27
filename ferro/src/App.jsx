961fbdfaf2f1a75a4b07aaf6bc6412cd56154e20
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-10-27 17:57:15 -0500

    Adding video route and component

--- ferro/src/App.jsx
+++ ferro/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header/Header"
-import Home from "./Containers/Home/Home"
-import Gallery from "./Containers/Gallery/Gallery"
+import Header from "./Components/Header/Header";
+import Home from "./Containers/Home/Home";
+import Gallery from "./Containers/Gallery/Gallery";
+import VideoGallery from "./Containers/VideoGallery/VideoGallery.jsx";
 import './App.scss'
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/gallery" element={<Gallery />}></Route>
+        <Route path="/videos" element={<VideoGallery />}></Route>
         
       </Routes>
     </BrowserRouter>
---
+++ ferro/src/Containers/VideoGallery/VideoGallery.jsx
@@ -0,0 +1,75 @@
+import { motion } from 'framer-motion';
+// import GalleryItems from "../../Constants/Images";
+import "./VideoGallery.scss"
+
+function VideoGallery() {
+
+    // const imageArray = Object.values(GalleryItems);
+
+    const videos = [
+        {
+          title: 'MIRAME OTRA VEZ',
+          description: 'Disfrutemos de momentos juntos! Estamos a un mensaje de distancia!',
+          videoUrl: 'https://www.youtube.com/embed/pvdZY4DbcHg?si=5N9v2qOjBUKmipOW',
+        },
+        {
+          title: 'CLANDESTINO',
+          description: 'Disfrutemos de momentos juntos! Estamos a un mensaje de distancia!',
+          videoUrl: 'https://www.youtube.com/embed/_1zwWmC1G4Q?si=OHNpwnJUKOEtwOYM',
+        },
+        {
+          title: 'HOY NO',
+          description: 'Disfrutemos de momentos juntos! Estamos a mensaje de distancia!',
+          videoUrl: 'https://www.youtube.com/embed/3vUxRmhIljs?si=R1JgDequdZbYaTdW',
+        },
+        {
+          title: 'SECRETO',
+          description: 'Disfrutemos de momentos juntos! Estamos a un mensaje de distancia!',
+          videoUrl: 'https://www.youtube.com/embed/vB8I-2uYd3o?si=PEddyQoq8pWkGhCX',
+        },
+      ];
+
+    return(
+
+        <motion.section className="gallery" id="galeria"
+        animate={{ x: [0, 100, 0] }}
+        
+        >
+            <motion.div className="container"
+                animate={{ x: [0, 100, 0] }}
+            >
+                <motion.h1 animate={{ x: [0, 100, 0] }}>Algunos de mis videos!</motion.h1>
+                <motion.div className="image-container"
+                    animate={{ x: [0, 100, 0] }}
+                >
+                    {videos.map((title, description, videoUrl, index) => (
+                        <motion.div className="image" key={index}
+                            whileHover={{
+                                scale: 3,
+                                border: "10px",
+                                transition: {
+                                    duration: .3,
+                                    ease: "easeInOut"
+                                }
+                            }}
+                            whileInView={{opacity: 1}}
+                        >
+                            <iframe key={`video number ${index}`}
+                                width='100%' 
+                                height='100%'
+                                frameBorder="0"
+                                src={videoUrl}
+                                title={title}
+                                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                                allowFullScreen
+                            ></iframe>
+                            <p>{description}</p>
+                        </motion.div> 
+                    ))}     
+                </motion.div>
+            </motion.div>
+        </motion.section>
+    )
+}
+
+export default VideoGallery;
