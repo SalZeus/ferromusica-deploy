48f9f3cb956d011d7027c6f75cf2996e28964a0c
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-11-04 10:22:07 -0500

    adding simplification on Gallery.jsx due to deployment errors

--- ferro/src/Containers/Gallery/Gallery.jsx
+++ ferro/src/Containers/Gallery/Gallery.jsx
@@ -1,8 +1,87 @@
 import { motion } from 'framer-motion';
-import GalleryItems from "../../constants/GalleryImages";
+// import GalleryItems from "../../constants/GalleryImages";
 import "./Gallery.scss"
 
 function Gallery() {
+
+    let cover = "https://imgur.com/3qVICyL";
+    let foto1 = "https://imgur.com/LxnDXri";
+    let foto2 = "https://imgur.com/1xJOlMb";
+    let foto3 = "https://imgur.com/dR1iXTM";
+    let foto4 = "https://imgur.com/aaNBrJG";
+    let foto5 = "https://imgur.com/fO3WTHi";
+    let foto6 = "https://imgur.com/Nq8R1Hi";
+    let foto7 = "https://imgur.com/sSjsdvT";
+    let foto8 = "https://imgur.com/YsNxQci";
+    let foto9 = "https://imgur.com/TO6MQjl";
+    let foto10=  "https://imgur.com/kPaIyMA";
+    let foto11=  "https://imgur.com/HAkhfLF";
+    let foto12=  "https://imgur.com/gK7XrAz";
+    let foto13=  "https://imgur.com/NDPt7sY";
+    let foto14=  "https://imgur.com/uw5VB8m";
+    let foto15=  "https://imgur.com/2mlhd0v";
+    let foto16=  "https://imgur.com/X7a1M1z";
+    let foto17=  "https://imgur.com/6qf6oN1";
+    let foto18=  "https://imgur.com/7i7AfnX";
+    let foto19=  "https://imgur.com/4LHXITj";
+    let foto20=  "https://imgur.com/rDOnBAK";
+    let foto21=  "https://imgur.com/mXqvO5E";
+    let foto22=  "https://imgur.com/BqvoT8e";
+    let foto23=  "https://imgur.com/xIAWTuk";
+    let foto24=  "https://imgur.com/mkukgU5";
+    let foto25=  "https://imgur.com/z9jl8dh";
+    let foto26=  "https://imgur.com/9THHbhi";
+    let foto27=  "https://imgur.com/3TN3jJw";
+    let foto28=  "https://imgur.com/xpOTnVV";
+    let foto29=  "https://imgur.com/ieZSaaj";
+    let foto30=  "https://imgur.com/2xqWJem";
+    let foto31=  "https://imgur.com/zAZzPba";
+    let foto32=  "https://imgur.com/zAZzPba";
+    let foto33=  "https://imgur.com/v0kB2Ok";
+    let foto34=  "https://imgur.com/oZJTL0o";
+    let foto35=  "https://imgur.com/qdVTnwl";
+
+
+
+    const GalleryItems = {
+
+    foto1,
+    foto2,
+    foto3,
+    foto4,
+    foto5,
+    foto6,
+    cover,
+    foto7,
+    foto8,
+    foto9,
+    foto10,
+    foto11,
+    foto12,
+    foto13,
+    foto14,
+    foto15,
+    foto16,
+    foto17,
+    foto18,
+    foto19,
+    foto20,
+    foto21,
+    foto22,
+    foto23,
+    foto24,
+    foto25,
+    foto26,
+    foto27,
+    foto28,
+    foto29,
+    foto30,
+    foto31,
+    foto32,
+    foto33,
+    foto34,
+    foto35,
+    }
 
     const imageArray = Object.values(GalleryItems);
 
